a1b0c63320fb9dfd0290d9cb221ec2d2a704ab36
AveSatan <misha81@yandex.ru>
2018-07-06 16:16:50 +0300

    wtf changes

--- src/com/core/Main.java
+++ src/com/core/Main.java
@@ -20,164 +20,157 @@ import com.sun.star.util.XReplaceable;
 
 public class Main {
 
-    public static void main(String args[]) {
-        // You need the desktop to create a document
-        // The getDesktop method does the UNO bootstrapping, gets the
-        // remote servie manager and the desktop object.
-        XDesktop xDesktop = null;
-        xDesktop = getDesktop();
+	public static void main(String args[]) {
+		// You need the desktop to create a document
+		// The getDesktop method does the UNO bootstrapping, gets the
+		// remote servie manager and the desktop object.
+		XDesktop xDesktop = null;
+		xDesktop = getDesktop();
 
-        XTextDocument xTextDocument =
-                createTextdocument( xDesktop );
+		XTextDocument xTextDocument =
+				createTextdocument(xDesktop);
 
-        createExampleData( xTextDocument );
+		createExampleData(xTextDocument);
 
-        String mBritishWords[] = {"colour", "neighbour", "centre", "behaviour",
-                "metre", "through" };
-        String mUSWords[] = { "color", "neighbor", "center", "behavior",
-                "meter", "thru" };
+		String mBritishWords[] = {"colour", "neighbour", "centre", "behaviour",
+				"metre", "through"};
+		String mUSWords[] = {"color", "neighbor", "center", "behavior",
+				"meter", "thru"};
 
-        XReplaceDescriptor xReplaceDescr = null;
-        XReplaceable xReplaceable = null;
+		XReplaceDescriptor xReplaceDescr = null;
+		XReplaceable xReplaceable = null;
 
-        xReplaceable = UnoRuntime.queryInterface(
-                XReplaceable.class, xTextDocument);
+		xReplaceable = UnoRuntime.queryInterface(
+				XReplaceable.class, xTextDocument);
 
-        // You need a descriptor to set properies for Replace
-        xReplaceDescr = xReplaceable.createReplaceDescriptor();
+		// You need a descriptor to set properies for Replace
+		xReplaceDescr = xReplaceable.createReplaceDescriptor();
 
-        System.out.println("Change all occurrences of ...");
-        for( int iArrayCounter = 0; iArrayCounter < mBritishWords.length;
-             iArrayCounter++ )
-        {
-            System.out.println(mBritishWords[iArrayCounter] +
-                    " -> " + mUSWords[iArrayCounter]);
-            // Set the properties the replace method need
-            xReplaceDescr.setSearchString(mBritishWords[iArrayCounter] );
-            xReplaceDescr.setReplaceString(mUSWords[iArrayCounter] );
+		System.out.println("Change all occurrences of ...");
+		for (int iArrayCounter = 0; iArrayCounter < mBritishWords.length;
+		     iArrayCounter++) {
+			System.out.println(mBritishWords[iArrayCounter] +
+					" -> " + mUSWords[iArrayCounter]);
+			// Set the properties the replace method need
+			xReplaceDescr.setSearchString(mBritishWords[iArrayCounter]);
+			xReplaceDescr.setReplaceString(mUSWords[iArrayCounter]);
 
-            // Replace all words
-            xReplaceable.replaceAll( xReplaceDescr );
-        }
+			// Replace all words
+			xReplaceable.replaceAll(xReplaceDescr);
+		}
 
-        System.out.println("Done");
-        System.exit(0);
-    }
+		System.out.println("Done");
+		System.exit(0);
+	}
 
-    private static void createExampleData(
-            XTextDocument xTextDocument)
-    {
-        // Create textdocument and insert example text
-        XTextCursor xTextCursor = null;
+	private static void createExampleData(
+			XTextDocument xTextDocument) {
+		// Create textdocument and insert example text
+		XTextCursor xTextCursor = null;
 
-        try {
-            xTextCursor = xTextDocument.getText().createTextCursor();
-            XText xText = xTextDocument.getText();
+		try {
+			xTextCursor = xTextDocument.getText().createTextCursor();
+			XText xText = xTextDocument.getText();
 
-            xText.insertString( xTextCursor,
-                    "He nervously looked all around. Suddenly he saw his ", false );
+			xText.insertString(xTextCursor,
+					"He nervously looked all around. Suddenly he saw his ", false);
 
-            xText.insertString( xTextCursor, "neighbour ", true );
-            XPropertySet xCPS = UnoRuntime.queryInterface(
-                    XPropertySet.class, xTextCursor);
-            // Set the word blue
-            xCPS.setPropertyValue( "CharColor", 255);
-            // Go to last character
-            xTextCursor.gotoEnd(false);
-            xCPS.setPropertyValue( "CharColor", 0);
+			xText.insertString(xTextCursor, "neighbour ", true);
+			XPropertySet xCPS = UnoRuntime.queryInterface(
+					XPropertySet.class, xTextCursor);
+			// Set the word blue
+			xCPS.setPropertyValue("CharColor", 255);
+			// Go to last character
+			xTextCursor.gotoEnd(false);
+			xCPS.setPropertyValue("CharColor", 0);
 
-            xText.insertString( xTextCursor, "in the alley. Like lightening he darted off to the left and disappeared between the two warehouses almost falling over the trash can lying in the ", false  );
+			xText.insertString(xTextCursor, "in the alley. Like lightening he darted off to the left and disappeared between the two warehouses almost falling over the trash can lying in the ", false);
 
-            xText.insertString( xTextCursor, "centre ", true );
-            xCPS = UnoRuntime.queryInterface(
-                    XPropertySet.class, xTextCursor);
-            // Set the word blue
-            xCPS.setPropertyValue( "CharColor", 255);
-            // Go to last character
-            xTextCursor.gotoEnd(false);
-            xCPS.setPropertyValue( "CharColor", 0);
+			xText.insertString(xTextCursor, "centre ", true);
+			xCPS = UnoRuntime.queryInterface(
+					XPropertySet.class, xTextCursor);
+			// Set the word blue
+			xCPS.setPropertyValue("CharColor", 255);
+			// Go to last character
+			xTextCursor.gotoEnd(false);
+			xCPS.setPropertyValue("CharColor", 0);
 
-            xText.insertString( xTextCursor, "of the sidewalk.", false );
+			xText.insertString(xTextCursor, "of the sidewalk.", false);
 
-            xText.insertControlCharacter( xTextCursor,
-                    ControlCharacter.PARAGRAPH_BREAK, false );
-            xText.insertString( xTextCursor, "He tried to nervously tap his way along in the inky darkness and suddenly stiffened: it was a dead-end, he would have to go back the way he had come.", false );
+			xText.insertControlCharacter(xTextCursor,
+					ControlCharacter.PARAGRAPH_BREAK, false);
+			xText.insertString(xTextCursor, "He tried to nervously tap his way along in the inky darkness and suddenly stiffened: it was a dead-end, he would have to go back the way he had come.", false);
 
-            xTextCursor.gotoStart(false);
-        }
-        catch( Exception e) {
-            e.printStackTrace(System.err);
-        }
+			xTextCursor.gotoStart(false);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
 
-    }
+	}
 
-    private static XDesktop getDesktop() {
-        XDesktop xDesktop = null;
-        XMultiComponentFactory xMCF = null;
+	private static XDesktop getDesktop() {
+		XDesktop xDesktop = null;
+		XMultiComponentFactory xMCF = null;
 
-        try {
-            XComponentContext xContext = null;
+		try {
+			XComponentContext xContext = null;
 
-            // get the remote office component context
-            xContext = Bootstrap.bootstrap();
+			// get the remote office component context
+			xContext = Bootstrap.bootstrap();
 
-            // get the remote office service manager
-            xMCF = xContext.getServiceManager();
-            if( xMCF != null ) {
-                System.out.println("Connected to a running office ...");
+			// get the remote office service manager
+			xMCF = xContext.getServiceManager();
+			if (xMCF != null) {
+				System.out.println("Connected to a running office ...");
 
-                Object oDesktop = xMCF.createInstanceWithContext(
-                        "com.sun.star.frame.Desktop", xContext);
-                xDesktop = UnoRuntime.queryInterface(
-                        XDesktop.class, oDesktop);
-            }
-            else
-                System.out.println( "Can't create a desktop. No connection, no remote office servicemanager available!" );
-        } catch( Exception | BootstrapException e ) {
-            e.printStackTrace(System.err);
-            System.exit(1);
-        }
+				Object oDesktop = xMCF.createInstanceWithContext(
+						"com.sun.star.frame.Desktop", xContext);
+				xDesktop = UnoRuntime.queryInterface(
+						XDesktop.class, oDesktop);
+			} else
+				System.out.println("Can't create a desktop. No connection, no remote office servicemanager available!");
+		} catch (Exception | BootstrapException e) {
+			e.printStackTrace(System.err);
+			System.exit(1);
+		}
 
 
-        return xDesktop;
-    }
+		return xDesktop;
+	}
 
-    private static com.sun.star.text.XTextDocument createTextdocument(
-            XDesktop xDesktop)
-    {
-        XTextDocument aTextDocument = null;
+	private static com.sun.star.text.XTextDocument createTextdocument(
+			XDesktop xDesktop) {
+		XTextDocument aTextDocument = null;
 
-        XComponent xComponent = CreateNewDocument(xDesktop,
-                "swriter");
-        aTextDocument = UnoRuntime.queryInterface(
-                XTextDocument.class, xComponent);
+		XComponent xComponent = CreateNewDocument(xDesktop,
+				"swriter");
+		aTextDocument = UnoRuntime.queryInterface(
+				XTextDocument.class, xComponent);
 
-        return aTextDocument;
-    }
+		return aTextDocument;
+	}
 
 
-    private static XComponent CreateNewDocument(
-            XDesktop xDesktop,
-            String sDocumentType)
-    {
-        String sURL = "private:factory/" + sDocumentType;
+	private static XComponent CreateNewDocument(
+			XDesktop xDesktop,
+			String sDocumentType) {
+		String sURL = "private:factory/" + sDocumentType;
 
-        XComponent xComponent = null;
-        XComponentLoader xComponentLoader = null;
-        PropertyValue xEmptyArgs[] =
-                new PropertyValue[0];
+		XComponent xComponent = null;
+		XComponentLoader xComponentLoader = null;
+		PropertyValue xEmptyArgs[] =
+				new PropertyValue[0];
 
-        try {
-            xComponentLoader = UnoRuntime.queryInterface(
-                    XComponentLoader.class, xDesktop);
+		try {
+			xComponentLoader = UnoRuntime.queryInterface(
+					XComponentLoader.class, xDesktop);
 
-            xComponent  = xComponentLoader.loadComponentFromURL(
-                    sURL, "_blank", 0, xEmptyArgs);
-        }
-        catch( Exception e) {
-            e.printStackTrace(System.err);
-        }
+			xComponent = xComponentLoader.loadComponentFromURL(
+					sURL, "_blank", 0, xEmptyArgs);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
 
-        return xComponent ;
-    }
+		return xComponent;
+	}
 }
